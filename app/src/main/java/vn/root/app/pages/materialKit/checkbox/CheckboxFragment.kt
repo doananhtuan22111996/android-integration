08316f794177516f43cb3ec9f64f86556336d0f6
Jason Đoàn <35907749+doananhtuan22111996@users.noreply.github.com>
2024-05-06 16:49:01 +0700

    feat: Theme - Material Design - Style - Checkbox (#16)

---
+++ app/src/main/java/vn/root/app/pages/materialKit/checkbox/CheckboxFragment.kt
@@ -0,0 +1,34 @@
+package vn.root.app.pages.materialKit.checkbox
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import vn.root.app.R
+import vn.root.app.databinding.FragmentCheckboxBinding
+
+class CheckboxFragment : Fragment() {
+	
+	private lateinit var viewBinding: FragmentCheckboxBinding
+	private lateinit var navController: NavController
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+	): View {
+		viewBinding = FragmentCheckboxBinding.inflate(inflater, container, false)
+		navController = findNavController()
+		return viewBinding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewBinding.layoutHeader.toolbar.apply {
+			title = context.getString(R.string.checkbox)
+			setNavigationOnClickListener { navController.popBackStack() }
+			menu.clear()
+		}
+	}
+}
