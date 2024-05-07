8527b7963b8aeca86c272659413768d542cf622f
tuan.da <doananhtuan22111996@gmail.com>
2024-05-07 23:37:35 +0700

    feat: Animation - Hyperspace jump, rotate and cross fade

---
+++ app/src/main/java/vn/root/app/pages/animation/AnimationFragment.kt
@@ -0,0 +1,60 @@
+package vn.root.app.pages.animation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import vn.root.app.R
+import vn.root.app.databinding.FragmentMaterialKitBinding
+import vn.root.app.pages.materialKit.MaterialKit
+import vn.root.app.pages.materialKit.MaterialKitAdapter
+
+class AnimationFragment : Fragment() {
+	
+	private lateinit var viewBinding: FragmentMaterialKitBinding
+	private lateinit var navController: NavController
+	private lateinit var adapter: MaterialKitAdapter
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		adapter = MaterialKitAdapter(
+			kits = listOf(
+				MaterialKit(
+					id = 1,
+					header = getString(R.string.hyperspace_jump),
+					subHeader = getString(R.string.motion_makes_a_ui_expressive_and_easy_to_use),
+					action = { navController.navigate(R.id.action_animationFragment_to_hyperspaceJumpFragment) },
+				),
+				MaterialKit(
+					id = 2,
+					header = getString(R.string.crossfade),
+					subHeader = getString(R.string.motion_makes_a_ui_expressive_and_easy_to_use),
+					action = { navController.navigate(R.id.action_animationFragment_to_crossfadeFragment) },
+				),
+			)
+		)
+	}
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+	): View {
+		viewBinding = FragmentMaterialKitBinding.inflate(inflater, container, false)
+		navController = findNavController()
+		return viewBinding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewBinding.layoutHeader.toolbar.apply {
+			title = getString(R.string.animation)
+			setNavigationOnClickListener {
+				navController.popBackStack()
+			}
+			menu.clear()
+		}
+		viewBinding.rcView.adapter = adapter
+	}
+}
---
+++ app/src/main/java/vn/root/app/pages/animation/crossfade/CrossfadeFragment.kt
@@ -0,0 +1,53 @@
+package vn.root.app.pages.animation.crossfade
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import vn.root.app.R
+import vn.root.app.databinding.FragmentCrossfadeBinding
+
+class CrossfadeFragment : Fragment() {
+	
+	private lateinit var viewBinding: FragmentCrossfadeBinding
+	private lateinit var navController: NavController
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+	): View {
+		viewBinding = FragmentCrossfadeBinding.inflate(inflater, container, false)
+		navController = findNavController()
+		return viewBinding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewBinding.layoutHeader.toolbar.apply {
+			title = getString(R.string.crossfade)
+			menu.clear()
+			setNavigationOnClickListener { navController.popBackStack() }
+		}
+		
+		viewBinding.btnInvisible.setOnClickListener {
+			AnimationUtils.loadAnimation(requireContext(), R.anim.crossfade_invisible).also {
+				viewBinding.textView.apply {
+					visibility = View.INVISIBLE
+					startAnimation(it)
+				}
+			}
+		}
+		
+		viewBinding.btnVisible.setOnClickListener {
+			AnimationUtils.loadAnimation(requireContext(), R.anim.crossfade_visible).also {
+				viewBinding.textView.apply {
+					visibility = View.VISIBLE
+					startAnimation(it)
+				}
+			}
+		}
+	}
+}
