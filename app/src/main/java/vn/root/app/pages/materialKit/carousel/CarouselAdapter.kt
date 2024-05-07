8d4ccf9b5e3b31b3ed05cdf5a2ca0feecf9362f1
tuan.da <doananhtuan22111996@gmail.com>
2024-05-07 16:43:45 +0700

    feat: Theme - Material Styles - Carousel

---
+++ app/src/main/java/vn/root/app/pages/materialKit/carousel/CarouselAdapter.kt
@@ -0,0 +1,36 @@
+package vn.root.app.pages.materialKit.carousel
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import vn.root.app.R
+import vn.root.app.databinding.ItemCarouselBinding
+
+class CarouselAdapter : RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
+	
+	inner class CarouselViewHolder(private val binding: ItemCarouselBinding) :
+		RecyclerView.ViewHolder(binding.root) {
+		
+		fun bind() {
+			binding.imageView.load(R.drawable.im_material_kit, builder = {
+				transformations(RoundedCornersTransformation(28f))
+			})
+		}
+	}
+	
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselViewHolder {
+		return CarouselViewHolder(
+			ItemCarouselBinding.inflate(
+				LayoutInflater.from(parent.context), parent, false
+			)
+		)
+	}
+	
+	override fun getItemCount(): Int = 10
+	
+	override fun onBindViewHolder(holder: CarouselViewHolder, position: Int) {
+		holder.bind()
+	}
+}
