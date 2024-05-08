084803bc2f134deea679fdbf94f704c0db72a2e6
Jason Đoàn <35907749+doananhtuan22111996@users.noreply.github.com>
2024-05-09 00:42:25 +0700

    feat: Animation - Hyperspace jump, rotate and cross fade (#20)

---
+++ app/src/main/java/vn/root/app/pages/animation/hyperspaceJump/HyperspaceJumpFragment.kt
@@ -0,0 +1,44 @@
+package vn.root.app.pages.animation.hyperspaceJump
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import vn.root.app.R
+import vn.root.app.databinding.FragmentHyperspaceJumpBinding
+
+class HyperspaceJumpFragment : Fragment() {
+	
+	private lateinit var viewBinding: FragmentHyperspaceJumpBinding
+	private lateinit var navController: NavController
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+	): View {
+		viewBinding = FragmentHyperspaceJumpBinding.inflate(inflater, container, false)
+		navController = findNavController()
+		return viewBinding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		viewBinding.layoutHeader.toolbar.apply {
+			title = getString(R.string.hyperspace_jump)
+			setNavigationOnClickListener { navController.popBackStack() }
+			menu.clear()
+		}
+		
+		AnimationUtils.loadAnimation(requireContext(), R.anim.hyperspace_jump).also {
+			viewBinding.imageView.startAnimation(it)
+		}
+		
+		AnimationUtils.loadAnimation(requireContext(), R.anim.pendulum).also {
+			viewBinding.imageView2.startAnimation(it)
+		}
+	}
+}
--- app/src/main/java/vn/root/app/pages/boarding/BoardingFragment.kt
+++ app/src/main/java/vn/root/app/pages/boarding/BoardingFragment.kt
@@ -52,6 +52,14 @@ class BoardingFragment : Fragment() {
 			}
 		}
 		viewBinding.layoutContent3.let {
+			it.tvHeader.text = getString(R.string.animation)
+			it.tvSubHeader.text =
+				getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_sed_do_eiusmod_tempor)
+			it.root.setOnClickListener {
+				navController.navigate(R.id.action_boardingFragment_to_animationFragment)
+			}
+		}
+		viewBinding.layoutContent4.let {
 			it.tvHeader.text = getString(R.string.drawable)
 			it.tvSubHeader.text =
 				getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_sed_do_eiusmod_tempor)
