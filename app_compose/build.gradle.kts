5ca3a14a4229dc77cfa54c2d50fed57e996d0ea5
Jason Đoàn <35907749+doananhtuan22111996@users.noreply.github.com>
2024-05-21 23:51:39 +0700

    feat: Codelab - jecpack compose - Exercise 3 completed - state (#24)

--- app_compose/build.gradle.kts
+++ app_compose/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
 	implementation(libs.androidx.core.splashscreen)
 	implementation(libs.androidx.navigation.compose)
 	implementation(libs.androidx.window.size)
+	implementation(libs.androidx.viewmodel.compose)
 	
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
