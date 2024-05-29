05613b6587f55bbd979958646126b22dba5c0004
Jason Đoàn <35907749+doananhtuan22111996@users.noreply.github.com>
2024-05-30 00:50:35 +0700

    refactor: Network Bound Service - flow with onStart, onCompletion (#28)
    * refactor: Network Bound Service - flow with onStart, onCompletion
    
    * refactor: Network Bound Service - flow with onStart, onCompletion

--- data/src/main/java/vn/root/data/network/NetworkBoundService.kt
+++ data/src/main/java/vn/root/data/network/NetworkBoundService.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import retrofit2.Response
 import timber.log.Timber
 import vn.root.data.model.ObjectResponse
@@ -14,49 +16,51 @@ import vn.root.domain.model.TypeException
 import java.net.HttpURLConnection.HTTP_BAD_GATEWAY
 
 abstract class NetworkBoundService<RequestType, ResultType>(private val dispatcher: CoroutineDispatcher = Dispatchers.IO) {
-	
-	private val tag = NetworkBoundService::class.java.name
-	
-	/**
-	 * Follow Best Practice
-	 * https://github.com/android/architecture-components-samples/blob/main/GithubBrowserSample/app/src/main/java/com/android/example/github/repository/NetworkBoundResource.kt
-	 * */
-	fun build() = flow {
-		emit(ResultModel.Loading)
-		emit(fetchFromNetwork())
-		delay(200) // Small delay to ensure all of the value emitted by the flow is consumed
-		emit(ResultModel.Done)
-	}.flowOn(dispatcher)
-	
-	/**
-	 * This Func handle response from Network [fetchFromNetwork].
-	 */
-	private suspend fun fetchFromNetwork(): ResultModel<ResultType> {
-		Timber.i(tag, "Fetch data from network")
-		val apiResponse = onApi()
-		val result = if (apiResponse.isSuccessful) {
-			val body = apiResponse.body()
-			processResponse(body)
-		} else {
-			try {
-				val obj = Gson().fromJson(
-					apiResponse.errorBody()?.string(), ObjectResponse::class.java
-				)
-				ResultModel.AppException(
-					type = TypeException.Network(httpCode = apiResponse.code()),
-					message = obj.metadata?.message
-				)
-			} catch (e: Exception) {
-				ResultModel.AppException(
-					type = TypeException.Network(httpCode = HTTP_BAD_GATEWAY),
-					message = "Network Somethings wrong! -- ${e.message}"
-				)
-			}
-		}
-		return result
-	}
-	
-	abstract suspend fun onApi(): Response<ObjectResponse<RequestType>>
-	
-	abstract suspend fun processResponse(request: ObjectResponse<RequestType>?): ResultModel.Success<ResultType>
+
+    private val tag = NetworkBoundService::class.java.name
+
+    /**
+     * Follow Best Practice
+     * https://github.com/android/architecture-components-samples/blob/main/GithubBrowserSample/app/src/main/java/com/android/example/github/repository/NetworkBoundResource.kt
+     * */
+    fun build() = flow {
+        emit(fetchFromNetwork())
+        delay(200) // Small delay to ensure all of the value emitted by the flow is consumed
+    }.flowOn(dispatcher).onStart {
+        emit(ResultModel.Loading)
+    }.onCompletion {
+        emit(ResultModel.Done)
+    }
+
+    /**
+     * This Func handle response from Network [fetchFromNetwork].
+     */
+    private suspend fun fetchFromNetwork(): ResultModel<ResultType> {
+        Timber.i(tag, "Fetch data from network")
+        val apiResponse = onApi()
+        val result = if (apiResponse.isSuccessful) {
+            val body = apiResponse.body()
+            processResponse(body)
+        } else {
+            try {
+                val obj = Gson().fromJson(
+                    apiResponse.errorBody()?.string(), ObjectResponse::class.java
+                )
+                ResultModel.AppException(
+                    type = TypeException.Network(httpCode = apiResponse.code()),
+                    message = obj.metadata?.message
+                )
+            } catch (e: Exception) {
+                ResultModel.AppException(
+                    type = TypeException.Network(httpCode = HTTP_BAD_GATEWAY),
+                    message = "Network Somethings wrong! -- ${e.message}"
+                )
+            }
+        }
+        return result
+    }
+
+    abstract suspend fun onApi(): Response<ObjectResponse<RequestType>>
+
+    abstract suspend fun processResponse(request: ObjectResponse<RequestType>?): ResultModel.Success<ResultType>
 }
