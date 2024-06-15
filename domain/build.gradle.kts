9b2c0d3dea3521549324676064fb3519810020d6
tuan.da <doananhtuan22111996@gmail.com>
2024-06-16 01:02:15 +0700

    [Domain] convert domain to android library for test

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,15 +1,45 @@
 plugins {
-	id("java-library")
-	alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "vn.root.domain"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
-	implementation(libs.androidx.core.coroutines)
-	implementation(libs.androidx.paging.common)
-	compileOnly(libs.javax)
-}
+    implementation(libs.androidx.core.coroutines)
+    implementation(libs.androidx.paging.common)
+    compileOnly(libs.javax)
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito)
+    testImplementation(libs.mockito.kotlin)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
